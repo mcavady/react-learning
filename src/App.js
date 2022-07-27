86a1e369b2f18e7f15a71f7ea441b687b992c226
James Mcavady <james@jsmcavady.co.uk>
2022-07-27 01:06:27 +0100

    Repo creation and starting with webstorm

--- src/App.js
+++ src/App.js
@@ -9,6 +9,9 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <p>
+          Perhaps this is something you could learn
+        </p>
         <a
           className="App-link"
           href="https://reactjs.org"
